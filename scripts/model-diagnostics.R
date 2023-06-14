3cf5691fb8b6b8463df9e71f94614597009648fa
Felipe Figueiredo <philsf79@gmail.com>
2023-06-14 20:43:38 -0300

    save CAUSE martingale residuals

--- scripts/model-diagnostics.R
+++ scripts/model-diagnostics.R
@@ -72,8 +72,10 @@ res.null.m <- resid(mod.null, type = "martingale")
 
 # Martingale: covariates against null -------------------------------------
 
+png("figures/diag_null_cause-mar.png")
 plot(md$Cause, res.null.m, ylab = "Martingale residuals")
 lines(lowess(md$Cause, res.null.m, iter = 0), lty = 2, col = "blue", lwd = 2)
+dev.off()
 
 png("figures/diag_null-mar.png", h = h, w = 3*w)
 par(mfrow = c(1,3))
