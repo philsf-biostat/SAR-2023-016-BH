7d6c45c405b7022368f6cd1220386e7047bc4467
Felipe Figueiredo <philsf79@gmail.com>
2024-10-07 13:24:35 -0300

    save Schoenfeld tests for all cutpoints + latest valid cutpoint (4mo)

--- scripts/manuscript3.R
+++ scripts/manuscript3.R
@@ -13,20 +13,37 @@ source("~/Documents/Consultoria/2023/SAR/SAR-2023-016-BH/scripts/modeling.R")
 tbl_merge(list(model4.1 %>% tab, model5.2 %>% tab), c("Model 4 early", "Model 5 late"))
 tbl_merge(list(model4.1 %>% tab, model5.2 %>% tab), c("Model 4 early", "Model 5 late")) %>% as_gt() %>% gtsave("~/Downloads/Brennan delivery/tab_inf_2mo.png")
 
+sch.df1 # none
+sch.df2 # rehab, FIM MOT
+writexl::write_xlsx(list("early" = sch.df1, "late" = sch.df2), "dataset/schoenfeld-2mo.xlsx")
+
 12*.25 # 3 months
 cutpoint <- .25
 source("~/Documents/Consultoria/2023/SAR/SAR-2023-016-BH/scripts/modeling.R")
 tbl_merge(list(model4.1 %>% tab, model5.2 %>% tab), c("Model 4 early", "Model 5 late"))
 tbl_merge(list(model4.1 %>% tab, model5.2 %>% tab), c("Model 4 early", "Model 5 late")) %>% as_gt() %>% gtsave("~/Downloads/Brennan delivery/tab_inf_3mo.png")
 
+sch.df1 # none
+sch.df2 # empl, FIM MOT
+writexl::write_xlsx(list("early" = sch.df1, "late" = sch.df2), "dataset/schoenfeld-3mo.xlsx")
+
 12*.333 # 4 months
 cutpoint <- .333
 source("~/Documents/Consultoria/2023/SAR/SAR-2023-016-BH/scripts/modeling.R")
 tbl_merge(list(model4.1 %>% tab, model5.2 %>% tab), c("Model 4 early", "Model 5 late"))
 tbl_merge(list(model4.1 %>% tab, model5.2 %>% tab), c("Model 4 early", "Model 5 late")) %>% as_gt() %>% gtsave("~/Downloads/Brennan delivery/tab_inf_4mo.png")
+tbl_merge(list(model4.1 %>% tab(everything()), model5.2 %>% tab(everything())), c("Model 4 early", "Model 5 late")) %>% write_rds("~/Downloads/Brennan delivery/tab_inf_4mo.rds")
+
+sch.df1 # none
+sch.df2 # none
+writexl::write_xlsx(list("early" = sch.df1, "late" = sch.df2), "dataset/schoenfeld-4mo.xlsx")
 
 12*.5 # 6 months
 cutpoint <- .5
 source("~/Documents/Consultoria/2023/SAR/SAR-2023-016-BH/scripts/modeling.R")
 tbl_merge(list(model4.1 %>% tab, model5.2 %>% tab), c("Model 4 early", "Model 5 late"))
 tbl_merge(list(model4.1 %>% tab, model5.2 %>% tab), c("Model 4 early", "Model 5 late")) %>% as_gt() %>% gtsave("~/Downloads/Brennan delivery/tab_inf_6mo.png")
+
+sch.df1 # SCI
+sch.df2 # none
+writexl::write_xlsx(list("early" = sch.df1, "late" = sch.df2), "dataset/schoenfeld-6mo.xlsx")
